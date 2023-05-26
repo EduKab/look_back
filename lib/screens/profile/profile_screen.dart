924845108403e7ea510a62702611274c146d7b57
David Portador <62720642+DavidPortador@users.noreply.github.com>
2023-05-26 10:43:24 -0600

    notifications background siiiuuuuuuuu

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,6 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:look_back/components/background.dart';
 import 'package:look_back/settings/responsive.dart';
+import 'dart:io';
+
+File? _image;
+final FirebaseStorage storage = FirebaseStorage.instance;
+final docProduct = FirebaseFirestore.instance.collection('products').doc();
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key, required this.data});
@@ -48,6 +56,31 @@ class ProfileData extends StatefulWidget {
 }
 
 class _ProfileDataState extends State<ProfileData> {
+
+  final _picker = ImagePicker();
+
+  // Implementing the gallery
+  Future<void> _openImagePicker() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  // Implementing the camera
+  Future<void> _openCameraPicker() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.camera);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -55,16 +88,54 @@ class _ProfileDataState extends State<ProfileData> {
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          CircleAvatar(
-            backgroundImage: NetworkImage(widget.data[1]!),
-            radius: 40,
-            backgroundColor: Colors.transparent,
+          // CircleAvatar(
+          //   backgroundImage: NetworkImage(widget.data[1]!),
+          //   radius: 40,
+          //   backgroundColor: Colors.transparent,
+          // ),
+          Container(
+            alignment: Alignment.center,
+            width: 200,
+            height: 200,
+            color: Colors.black26,
+            child: _image != null
+                ? Image.file(
+                    _image!,
+                    fit: BoxFit.cover,
+                  )
+                : Image.network(widget.data[1]!)
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton.icon(
+                  onPressed: _openImagePicker,
+                  icon: const Icon(Icons.add_photo_alternate_rounded),
+                  label: const Text('Gallery'),
+                ),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: ElevatedButton.icon(
+                  onPressed: _openCameraPicker,
+                  icon: const Icon(Icons.add_a_photo_rounded),
+                  label: const Text('Camera'),
+                ),
+              ),
+            ],
           ),
           const SizedBox(height: 20),
           Text(widget.data[0]!),
           const SizedBox(height: 20),
           Text(widget.data[2]!),
           const SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: () {
+              
+            },
+            icon: const Icon(Icons.image),
+            label: const Text('Save photo')),
+            const SizedBox(height: 20),
           ElevatedButton.icon(
             onPressed: () {
               Navigator.pop(context);
